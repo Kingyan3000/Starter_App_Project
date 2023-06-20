cd75788800f77cb9e50a9200c619ebad4bbb58ee
shayan <shayan.bagdeli@outlook.com>
2023-06-20 23:10:43 +0330

    Programming Concept - inheritance.

--- app/src/main/java/com/training/starterappproject/MainActivity.java
+++ app/src/main/java/com/training/starterappproject/MainActivity.java
@@ -11,7 +11,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Project[] projects = {
+                new Project( "Getting Started app","Our very first project, the default 'Hello World' app!", R.drawable.getting_started),
+                new Project( "App","Making a simple change to the layout, with my oun personal motivational quote for software developers!", R.drawable.quote),
+                new Project( "BMI Calculator","A real life working BMI calculator teaching me Variables, Methods and Conditional logic ", R.drawable.calculator),
+                new Project( "Inches Converter","A basic converter to convert inches to meters (my own personal calculator app made on my own)", R.drawable.tape),
+                new Project( "The Hungry Developer","A menu app for a fictional restaurant, learning abut Activities, Classes & Objects,Arrays Intends and ListViews", R.drawable.hungry_developer)
 
+
+
+        };
 
     }
 
--- app/src/main/java/com/training/starterappproject/Project.java
+++ app/src/main/java/com/training/starterappproject/Project.java
@@ -6,15 +6,6 @@ public class Project {
     String description;
     int image;
 
-//    Project(String name, String description, int image){
-//        this.name = name;
-//        this.description = description;
-//        this.image = image;
-//    }
-
-    // R.drawable.name_of_the_file (this will be some sort of number, an 'int')
-    // e.g. 15237475
-
 
     public Project(String name, String description, int image) {
         this.name = name;
